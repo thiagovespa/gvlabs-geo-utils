c4889251a3066d039232ff6dd59f3838c527a1b7
Thiago Galbiatti Vespa <thiagovespa@gvlabs.org>
2012-10-18 17:17:13 -0300

    Comments

--- gvlabs-geo-utils/src/org/gvlabs/utils/geo/format/SimpleGeoCoordinateFormat.java
+++ gvlabs-geo-utils/src/org/gvlabs/utils/geo/format/SimpleGeoCoordinateFormat.java
@@ -20,12 +20,22 @@ public class SimpleGeoCoordinateFormat extends Format {
 	 */
 	private static final long serialVersionUID = 201210161409L;
 	
+	private static final double SIXTH = 60.0;
+	
 	private GeoType geoType;
 
+	/**
+	 * Formatter constructor
+	 * 
+	 * @param geoType latitude or longitude
+	 */
 	public SimpleGeoCoordinateFormat(GeoType geoType) {
 		this.geoType = geoType;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.text.Format#format(java.lang.Object, java.lang.StringBuffer, java.text.FieldPosition)
+	 */
 	@Override
 	public StringBuffer format(Object obj, StringBuffer toAppendTo,
 			FieldPosition pos) {
@@ -35,8 +45,8 @@ public class SimpleGeoCoordinateFormat extends Format {
 		if (obj instanceof Number) {
 			double value = ((Number) obj).doubleValue();
 			degree = (int) value;
-			minutes = (int) (value - degree * 60.0);
-			seconds = (int) (value - (60.0 * (degree * 60 + minutes)));
+			minutes = (int) (value - degree * SIXTH);
+			seconds = (int) (value - (SIXTH * (degree * SIXTH + minutes)));
 			toAppendTo.append(degree).append("\u00BA ").append(minutes)
 					.append("\" ").append(seconds).append("\' ");
 			if(GeoType.LONGITUDE.equals(geoType)) {
@@ -57,6 +67,9 @@ public class SimpleGeoCoordinateFormat extends Format {
 		return toAppendTo;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.text.Format#parseObject(java.lang.String, java.text.ParsePosition)
+	 */
 	@Override
 	public Object parseObject(String source, ParsePosition pos) {
 		double degreeValue = 0.0;
